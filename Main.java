347f98120d87ecdbac852a354ad4216aa1b568d4
lancepr1 <108348802+lancepr1@users.noreply.github.com>
2024-09-11 13:13:32 +0000

    main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 3;
+        double b = 4.0;
+        boolean l = true;
+        String word = "Hello";
+        final double PI = 3.14159;
+        double ab = a * b;
+        a = 5;
+        System.out.println("Multiplication of int and double: " + ab);
+        System.out.println("Concatenation " + ab + l + word);
+        System.out.println("Pi:" + PI);
+    }
+}
