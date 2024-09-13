5a24d6dc01f0084e22f79e6197e8c7dc6ab53d82
lancepr1 <108348802+lancepr1@users.noreply.github.com>
2024-09-13 13:14:16 +0000

    hw 1.3

--- Main.java
+++ Main.java
@@ -1,14 +1,42 @@
-public class Main {
-    public static void main(String[] args){
-        int a = 3;
-        double b = 4.0;
-        boolean l = true;
-        String word = "Hello";
-        final double PI = 3.14159;
-        double ab = a * b;
-        a = 5;
-        System.out.println("Multiplication of int and double: " + ab);
-        System.out.println("Concatenation " + ab + l + word);
-        System.out.println("Pi:" + PI);
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welome to my calculator!");
+        System.out.println("Enter your first number.");
+        Scanner s = new Scanner(System.in);
+        int first = s.nextInt();
+        System.out.println("What operation would you like to do?");
+        String operation = s.next();
+        System.out.println("Enter your second number.");
+        int second = s.nextInt();
+        if(operation.equals("+"))
+        {
+            System.out.println("Answer: " + (first + second));
+        }
+        else if(operation.equals("-"))
+        {
+            System.out.println("Answer: " + (first - second));
+        }
+        else if(operation.equals("*"))
+        {
+            System.out.println("Answer: " + (first*second));
+        }
+        else if(operation.equals("/"))
+        {
+            if(second == 0){
+                System.out.println("Error, cannot divide by zero");
+            }
+            else{
+                System.out.println("Answer: " + (first/second));
+            }
+
+        }
+        else if(operation.equals("%"))
+        {
+            System.out.println("Answer: " + (first%second));
+        }
+        s.close();
     }
 }
